dcfeccc5a5c7c4dccd6dcfc0bcb2629b7117af64
Michel Morin <michel.morin@ouest-france.fr>
2022-01-25 11:07:49 +0100

    modif migrate-helm.sh

--- scripts/migrate_helm.sh
+++ scripts/migrate_helm.sh
@@ -20,25 +20,28 @@ if [ "$RELEASE" == "" -o "$NAMESPACE" == "" ]; then
    usage
 fi
 
-RELEASEV2=$(helm2 --tiller-namespace ${NAMESPACE} list -q "^${RELEASE}$")
+RELEASEV3=$(helm3 list -qn ${NAMESPACE} -f "^${RELEASE}$")
 if [ ! $? -eq 0 ]; then
    echo "Erreur dans la migration de la release ${RELEASE}"
    exit 2
 fi
-if [ "$RELEASEV2" != "" ]; then
-
-   # Migration release
-   helm3 2to3 convert -t ${NAMESPACE} ${RELEASE}
-   if [ ! $? -eq 0 ]; then
-      echo "Erreur dans la migration de la release ${RELEASE}"
-      exit 2
-   fi
+if [ "$RELEASEV3" == "" ]; then
+   # Test s'il y a une release V2. Sinon, c'est une nouvelle release
+   RELEASEV2=$(helm2 --tiller-namespace ${NAMESPACE} list -q "^${RELEASE}$")
+   if [ "$RELEASEV2" != "" ]; then
+      # Migration release
+      helm3 2to3 convert -t ${NAMESPACE} ${RELEASE}
+      if [ ! $? -eq 0 ]; then
+         echo "Erreur dans la migration de la release ${RELEASE}"
+         exit 2
+      fi
   
-   RELEASEV3=$(helm3 list -qn ${NAMESPACE} -f "^${RELEASE}$")
-   if [  ! $? -eq 0 -o "$RELEASEV3" == "" ]; then
-      echo "Erreur dans la migration de la release ${RELEASE}"
-      exit 3
+      RELEASEV3=$(helm3 list -qn ${NAMESPACE} -f "^${RELEASE}$")
+      if [  ! $? -eq 0 -o "$RELEASEV3" == "" ]; then
+         echo "Erreur dans la migration de la release ${RELEASE}"
+         exit 3
+      fi
+      echo "Release $2 migree en Helm3"
+      exit 1
    fi
-   echo "Release $2 migree en Helm3"
-   exit 1
 fi
