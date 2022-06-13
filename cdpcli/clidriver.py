2e29f1e55dd9c020c6971a81a317fcc847aa6ed6
Michel Morin <michel.morin@ouest-france.fr>
2022-06-13 11:08:49 +0200

    correction cleanup

--- cdpcli/clidriver.py
+++ cdpcli/clidriver.py
@@ -629,7 +629,7 @@ class CLIDriver(object):
 
         # Tout s'est bien passé, on clean la release ou le namespace si dernière release
         if cleanupHelm2:
-           self._cmd.run_command("/cdp/scripts/cleanup.sh %s -r %s" % ("-t " + namespace if self._context.opt['--tiller-namespace'] else "", release))            
+           self._cmd.run_command("/cdp/scripts/cleanup.sh %s -r %s" % ("-n " + namespace if self._context.opt['--tiller-namespace'] else "", release))            
 
         self.__update_environment()
 
